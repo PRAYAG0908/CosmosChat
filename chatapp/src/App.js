be6393d8e0cf945240670c845c226392513d5d38
prayag <prayag.pac@gmail.com>
2023-12-28 13:59:25 +0530

    Initial Requirements

---
+++ chatapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    
+    <div>
+       <h1>Hello</h1>
+      <img src="/images/welcome1.jpg" alt='welcome'/>
+    </div>
+  );
+}
+
+export default App;
